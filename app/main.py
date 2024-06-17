6b95a7f78f61d8aabfe67d4b7a36237af088d719
Mahendar <mahendar@lima.ai>
2024-06-17 18:29:10 +0530

    first

--- app/main.py
+++ app/main.py
@@ -6,9 +6,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-import uvicorn
-
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
